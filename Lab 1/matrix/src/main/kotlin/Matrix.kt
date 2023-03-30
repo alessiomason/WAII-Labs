72df9c3873aa5c6b1647161928bf21b272736534
Alessio Mason <alessio.mason@me.com>
2023-03-30 10:36:48 +0200

    Lab 1: Matrix fix

--- Lab 1/matrix/src/main/kotlin/Matrix.kt
+++ Lab 1/matrix/src/main/kotlin/Matrix.kt
@@ -1,12 +1,12 @@
-class Matrix(private val matrixAsString: String) {
-
+class Matrix(matrixAsString: String) {
     private val m = matrixAsString.lines() // remove \n or other delimitation characters
-        .map{ it.split(' ') // split('') -> list of string divided by row
+            .map{ it.split(' ') // split(' ') -> list of string divided by row
             .map(String::toInt) // cast to Int
         }
 
     fun column(colNr: Int): List<Int> {
-        return m.map { r -> r[colNr -1 ] } // for each row at "colNr - 1" index, take only the first value (row and column start by 0)
+        // for each row at "colNr - 1" index, take only the first value (row and column start by 0)
+        return m.map { r -> r[colNr - 1] }
     }
 
     fun row(rowNr: Int): List<Int> {
